357c6a62a222f6419be0b47d860c4f1e33107252
master-stm <twisterleader@yahoo.com>
2020-09-25 03:05:26 +0200

    add cookie-parser

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const express = require('express')
-// const ejs = require('ejs')
+const ejs = require('ejs')
 const mongoose = require('mongoose')
 const jwt = require('jsonwebtoken')
 const User = require('./models/userModel.js')
-const { render } = require('ejs')
+const cookieParser = require('cookie-parser')
 
 const app = express()
 
@@ -13,11 +13,12 @@ mongoose.connection.on('connected', () => console.log('database connected'))
 app.set('view engine', 'ejs')
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
-
+app.use(cookieParser()); //to parse cookies 
 
 
 //get  http://localhost:5000
 app.get('/', (req, res) => {
+    console.log(req.cookies.token);
     res.render('index')
 })
 
@@ -35,8 +36,8 @@ app.post('/user', (req, res) => {
     })
 
     const token = jwt.sign({ NewUser }, 'secretKEY')
-    console.log(token);
-    res.cookie(token)
+    
+    res.cookie('token',token)
     res.render('users', { NewUser })
 })
 
